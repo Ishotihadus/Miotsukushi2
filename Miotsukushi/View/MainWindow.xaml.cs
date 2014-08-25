a71cbd8918eaf0d4d35bd4e6a60f58321db8a26e
Ishotihadus <hanacan.pao@gmail.com>
2014-08-26 00:26:22 +0900

    MVVM構築

--- Miotsukushi/View/MainWindow.xaml.cs
+++ Miotsukushi/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Miotsukushi
+namespace Miotsukushi.View
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
