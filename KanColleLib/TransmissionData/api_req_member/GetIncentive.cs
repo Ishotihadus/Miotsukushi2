c7c748fb474baa4022b4af60571f90b51a1d4f50
Ishotihadus <hanacan.pao@gmail.com>
2014-08-30 09:11:58 +0900

    api_req_member/get_incentiveを追加
    だれかこのデータの意味を教えてくれ

---
+++ KanColleLib/TransmissionData/api_req_member/GetIncentive.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanColleLib.TransmissionData.api_req_member
+{
+    public class GetIncentive
+    {
+        /// <summary>
+        /// 不明
+        /// </summary>
+        public int count;
+
+        public GetIncentive fromDynamic(dynamic json)
+        {
+            return new GetIncentive() { count = (int)json.api_count };
+        }
+    }
+}
