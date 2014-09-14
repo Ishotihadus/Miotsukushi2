7bacf5c778472f161aa94c6923334839d8849f2c
Ishotihadus <hanacan.pao@gmail.com>
2014-09-14 22:11:07 +0900

    詳細情報パネルのプロトタイプ実そう

---
+++ Miotsukushi/ViewModel/DetailInfoPanel/DetailInfoPanelViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miotsukushi.ViewModel.DetailInfoPanel
+{
+    class DetailInfoPanelViewModel : ViewModelBase
+    {
+        public List<string> TabTitle { get; private set; }
+
+        public DetailInfoPanelViewModel()
+        {
+            TabTitle = new List<string>()
+            {
+                "総合",
+                "第1艦隊",
+                "第2艦隊",
+                "第3艦隊",
+                "第4艦隊",
+                "遠征"
+            };
+        }
+    }
+}
