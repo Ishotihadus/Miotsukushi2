a46ab9411239e278b9e64913c5a38fa217013ef4
Ishotihadus <hanachan.pao@gmail.com>
2015-08-04 22:22:28 +0900

    Battleのリクエスト実装

---
+++ KanColleLib/TransmissionRequest/api_req_sortie/BattleRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanColleLib.TransmissionRequest.api_req_sortie
+{
+    public class BattleRequest : RequestBase
+    {
+        public int recovery_type;
+        public int formation;
+
+        public BattleRequest(string request) : base(request)
+        {
+            recovery_type = _Get_Request_int("api_recovery_type").Value;
+            formation = _Get_Request_int("api_formation").Value;
+        }
+    }
+}
