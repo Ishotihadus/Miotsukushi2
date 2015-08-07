75d8342ceb19aa45edf26bdd5b610cf353c6c847
Ishotihadus <hanachan.pao@gmail.com>
2015-08-07 17:09:10 +0900

    艦隊概況でのキラキラ表示を実装

---
+++ Miotsukushi/Converter/IsKirakiraVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Miotsukushi.Converter
+{
+    class IsKirakiraVisibilityConverter : IValueConverter
+    {
+        public IsKirakiraVisibilityConverter()
+        {
+
+        }
+
+        public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is int && (int)value > 49)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
