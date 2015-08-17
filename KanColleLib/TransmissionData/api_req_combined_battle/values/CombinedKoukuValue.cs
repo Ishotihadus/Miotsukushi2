418ca23c9b2bc984b8e9c216fcab34737f31df5c
Ishotihadus <hanachan.pao@gmail.com>
2015-08-17 16:58:57 +0900

    連合水上艦隊昼戦戦闘json（api_req_combined/battle_water）ラッパー追加

---
+++ KanColleLib/TransmissionData/api_req_combined_battle/values/CombinedKoukuValue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanColleLib.TransmissionData.api_req_combined_battle.values
+{
+    public class CombinedKoukuValue : KanColleLib.TransmissionData.api_req_sortie.values.KoukuValue
+    {
+        /// <summary>
+        /// ステージ3（雷撃・爆撃）の連合艦隊随伴艦状況
+        /// </summary>
+        public KoukuStage3Combined stage3_combined;
+
+        new public static CombinedKoukuValue fromDynamic(dynamic json)
+        {
+            return new CombinedKoukuValue()
+            {
+                plane_from = json.api_plane_from.Deserialize<int[][]>(),
+                stage1 = json.api_stage1() && json.api_stage1 != null ? KanColleLib.TransmissionData.api_req_sortie.values.KoukuStage1.fromDynamic(json.api_stage1) : null,
+                stage2 = json.api_stage2() && json.api_stage2 != null ? KanColleLib.TransmissionData.api_req_sortie.values.KoukuStage2.fromDynamic(json.api_stage2) : null,
+                stage3 = json.api_stage3() && json.api_stage3 != null ? KanColleLib.TransmissionData.api_req_sortie.values.KoukuStage3.fromDynamic(json.api_stage3) : null,
+                stage3_combined = json.api_stage3_combined() && json.api_stage3_combined != null ? KoukuStage3Combined.fromDynamic(json.api_stage3_combined) : null
+            };
+        }
+    }
+}
---
+++ KanColleLib/TransmissionData/api_req_combined_battle/values/KoukuStage3Combined.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanColleLib.TransmissionData.api_req_combined_battle.values
+{
+    public class KoukuStage3Combined
+    {
+        /// <summary>
+        /// 随伴艦隊の被雷撃状況（1から始まる）
+        /// </summary>
+        public bool[] frai_flag;
+
+        /// <summary>
+        /// 随伴艦隊の被爆撃状況（1から始まる）
+        /// </summary>
+        public bool[] fbak_flag;
+
+        /// <summary>
+        /// 随伴艦隊のクリティカル等情報（1から始まる）
+        /// </summary>
+        public int[] fcl_flag;
+
+        /// <summary>
+        /// 随伴艦隊の受けたダメージ（1から始まる）
+        /// </summary>
+        public double[] fdam;
+
+        public static KoukuStage3Combined fromDynamic(dynamic json)
+        {
+            return new KoukuStage3Combined()
+            {
+                frai_flag = ((int[])(json.api_frai_flag.Deserialize<int[]>())).Select(_ => _ == 1).ToArray(),
+                fbak_flag = ((int[])(json.api_fbak_flag.Deserialize<int[]>())).Select(_ => _ == 1).ToArray(),
+                fcl_flag = json.api_fcl_flag.Deserialize<int[]>(),
+                fdam = json.api_fdam.Deserialize<double[]>(),
+            };
+        }
+    }
+}
