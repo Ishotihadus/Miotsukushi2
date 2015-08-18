bfd1b4a35b92cd461f49e09c09657d994b645ede
Ishotihadus <hanachan.pao@gmail.com>
2015-08-18 21:01:16 +0900

    destroyitem2のラッパーを追加

---
+++ KanColleLib/TransmissionData/api_req_kousyou/Destroyitem2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanColleLib.TransmissionData.api_req_kousyou
+{
+    public class Destroyitem2
+    {
+        /// <summary>
+        /// 得られる資材（燃料/弾薬/鋼材/ボーキ）
+        /// </summary>
+        public int[] get_material;
+
+        public static Destroyitem2 fromDynamic(dynamic json)
+        {
+            return new Destroyitem2()
+            {
+                get_material = json.api_get_material.Deserialize<int[]>()
+            };
+        }
+    }
+}
---
+++ KanColleLib/TransmissionRequest/api_req_kousyou/Destroyitem2Request.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanColleLib.TransmissionRequest.api_req_kousyou
+{
+    public class Destroyitem2Request : RequestBase
+    {
+        public int[] slotitem_ids;
+
+        public Destroyitem2Request(string request)
+            : base(request)
+        {
+            slotitem_ids = _Get_Request_int_array("api_slotitem_ids").Select(_ => _.HasValue ? _.Value : 0).ToArray();
+        }
+    }
+}
