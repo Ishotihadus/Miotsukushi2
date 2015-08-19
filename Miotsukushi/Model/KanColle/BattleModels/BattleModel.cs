da43a5799c32791e5bddc999673a1af58c0fd3a2
Ishotihadus <hanachan.pao@gmail.com>
2015-08-19 09:04:07 +0900

    BattleModelのResultイベントに連合艦隊分も追加

--- Miotsukushi/Model/KanColle/BattleModels/BattleModel.cs
+++ Miotsukushi/Model/KanColle/BattleModels/BattleModel.cs
@@ -22,6 +22,7 @@ namespace Miotsukushi.Model.KanColle.BattleModels
             kclib.GetReqsortieBattleresult += Kclib_GetReqsortieBattleresult;
             kclib.GetReqsortieBattle += Kclib_GetReqsortieBattle;
             kclib.GetReqbattlemidnightBattle += Kclib_GetReqbattlemidnightBattle;
+            kclib.GetReqcombinedbattleBattleresult += Kclib_GetReqcombinedbattleBattleresult;
         }
 
         private void Kclib_GetReqbattlemidnightBattle(object sender, KanColleLib.TransmissionRequest.api_req_battle_midnight.BattleRequest request, KanColleLib.TransmissionData.Svdata<KanColleLib.TransmissionData.api_req_battle_midnight.Battle> response)
@@ -37,6 +38,16 @@ namespace Miotsukushi.Model.KanColle.BattleModels
         }
 
         private void Kclib_GetReqsortieBattleresult(object sender, KanColleLib.TransmissionRequest.RequestBase request, KanColleLib.TransmissionData.Svdata<KanColleLib.TransmissionData.api_req_sortie.Battleresult> response)
+        {
+            OnGetBattleResult(new GetBattleResultEventArgs()
+            {
+                rank = response.data.win_rank,
+                has_get_ship = response.data.get_flag[1],
+                get_ship_name = response.data.get_ship != null ? response.data.get_ship.ship_name : ""
+            });
+        }
+
+        private void Kclib_GetReqcombinedbattleBattleresult(object sender, KanColleLib.TransmissionRequest.RequestBase request, KanColleLib.TransmissionData.Svdata<KanColleLib.TransmissionData.api_req_combined_battle.Battleresult> response)
         {
             OnGetBattleResult(new GetBattleResultEventArgs()
             {
