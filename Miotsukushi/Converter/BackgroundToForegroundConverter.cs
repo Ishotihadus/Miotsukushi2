f026702b7916d8a638ac018e863c4f92b3e9f37f
Ishotihadus <hanachan.pao@gmail.com>
2015-08-22 12:05:41 +0900

    一部Viewの文字色を改良

---
+++ Miotsukushi/Converter/BackgroundToForegroundConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Miotsukushi.Converter
+{
+    class BackgroundToForegroundConverter : IValueConverter
+    {
+        private Color IdealTextColor(Color bg)
+        {
+            const int nThreshold = 105;
+            var bgDelta = System.Convert.ToInt32((bg.R * 0.299) + (bg.G * 0.587) + (bg.B * 0.114));
+            var foreColor = (255 - bgDelta < nThreshold) ? Colors.Black : Colors.White;
+            return foreColor;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is SolidColorBrush)
+            {
+                var idealForegroundColor = this.IdealTextColor(((SolidColorBrush)value).Color);
+                var foreGroundBrush = new SolidColorBrush(idealForegroundColor);
+                foreGroundBrush.Freeze();
+                return foreGroundBrush;
+            }
+            else
+            {
+                return Brushes.White;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
