3c9f3c56f5e18d3eccc49b82b0db622cdba32d87
Ishotihadus <hanachan.pao@gmail.com>
2015-08-27 18:22:42 +0900

    api_req_member/get_incentiveを実装

--- KanColleLib/TransmissionData/api_req_member/GetIncentive.cs
+++ KanColleLib/TransmissionData/api_req_member/GetIncentive.cs
@@ -14,10 +14,44 @@ namespace KanColleLib.TransmissionData.api_req_member
         public int count;
 
         // api_item があるという噂
+        public List<values.ItemValue> item;
 
         public static GetIncentive fromDynamic(dynamic json)
         {
-            return new GetIncentive() { count = (int)json.api_count };
+            var ret = new GetIncentive();
+            ret.count = (int)json.api_count;
+            ret.item = new List<values.ItemValue>();
+            if (json.item())
+                foreach (var item in json.item)
+                    ret.item.Add(values.ItemValue.fromDynamic(item));
+            return ret;
         }
+
+        /*
+https://twitter.com/andanteyk/status/636829185617477632
+
+{
+    "api_result": 1,
+    "api_result_msg": "成功",
+    "api_data": {
+        "api_count": 2,
+        "api_item": [
+            {
+                "api_mode": 3,
+                "api_type": 2,
+                "api_mst_id": 139,
+                "api_getmes": "七月作戦 主力艦隊第三群(101～500位)<br>15.2cm連装砲改 を獲得しました！"
+            },
+            {
+                "api_mode": 3,
+                "api_type": 2,
+                "api_mst_id": 148,
+                "api_getmes": "七月作戦 主力艦隊第三群(101～500位)<br>試製南山 を獲得しました！"
+            }
+        ]
+    }
+}
+
+        */
     }
 }
---
+++ KanColleLib/TransmissionData/api_req_member/values/ItemValue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanColleLib.TransmissionData.api_req_member.values
+{
+    public class ItemValue
+    {
+        /// <summary>
+        /// 1=事前登録?, 2=招待?, 3=月次褒賞/プレゼント
+        /// </summary>
+        public int mode;
+
+        /// <summary>
+        /// 1=艦船?, 2=装備, 3=アイテム, 4=資源?, 5=家具
+        /// </summary>
+        public int type;
+
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int mst_id;
+
+        /// <summary>
+        /// メッセージ
+        /// </summary>
+        public string getmes;
+
+        public static ItemValue fromDynamic(dynamic json)
+        {
+            return new ItemValue()
+            {
+                mode = (int)json.api_mode,
+                type = (int)json.api_type,
+                mst_id = (int)json.api_mst_id,
+                getmes = json.api_getmes as string
+            };
+        }
+    }
+}
