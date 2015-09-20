f4dc9f0321c222963f888a7eb9e85669024a6a6b
Ishotihadus <hanachan.pao@gmail.com>
2015-09-20 14:44:06 +0900

    アバウト画面を実装

---
+++ Miotsukushi/View/CheatWindow/Components/AboutView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Miotsukushi.ViewModel.CheatWindow;
+
+namespace Miotsukushi.View.CheatWindow.Components
+{
+    /// <summary>
+    /// About.xaml の相互作用ロジック
+    /// </summary>
+    public partial class AboutView : UserControl
+    {
+        public AboutView()
+        {
+            InitializeComponent();
+            RenderOptions.SetBitmapScalingMode(Mascotimage, BitmapScalingMode.HighQuality);
+
+            try
+            {
+                DataContext = new AboutViewModel();
+            }
+            catch
+            {
+            }
+        }
+    }
+}
---
+++ Miotsukushi/ViewModel/CheatWindow/AboutViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Miotsukushi.ViewModel.CheatWindow
+{
+    class AboutViewModel : ViewModelBase
+    {
+        private string _version;
+        public string Version
+        {
+            get
+            {
+                return _version;
+            }
+
+            set
+            {
+                if (_version != value)
+                {
+                    _version = value;
+                    OnPropertyChanged(() => Version);
+                }
+            }
+        }
+
+        public AboutViewModel()
+        {
+            var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            Version = version.ToString();
+        }
+    }
+}
