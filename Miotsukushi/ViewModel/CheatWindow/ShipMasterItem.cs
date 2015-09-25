847fe0c60db86d8f461cec0798b4cfd28b73b4d7
Ishotihadus <hanachan.pao@gmail.com>
2015-09-25 16:20:31 +0900

    チートシートの艦娘マスター一覧に検索機能を追加

---
+++ Miotsukushi/ViewModel/CheatWindow/ShipMasterItem.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miotsukushi.ViewModel.CheatWindow
+{
+    class ShipMasterItem : ViewModelBase
+    {
+        private int _id;
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged(() => Id);
+                }
+            }
+        }
+
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(() => Name);
+                }
+            }
+        }
+
+
+        private string _shipType;
+        public string ShipType
+        {
+            get
+            {
+                return _shipType;
+            }
+
+            set
+            {
+                if (_shipType != value)
+                {
+                    _shipType = value;
+                    OnPropertyChanged(() => ShipType);
+                }
+            }
+        }
+
+
+        private int _shipTypeId;
+        public int ShipTypeId
+        {
+            get
+            {
+                return _shipTypeId;
+            }
+
+            set
+            {
+                if (_shipTypeId != value)
+                {
+                    _shipTypeId = value;
+                    OnPropertyChanged(() => ShipTypeId);
+                }
+            }
+        }
+
+
+        private string _yomi;
+        public string Yomi
+        {
+            get
+            {
+                return _yomi;
+            }
+
+            set
+            {
+                if (_yomi != value)
+                {
+                    _yomi = value;
+                    OnPropertyChanged(() => Yomi);
+                }
+            }
+        }
+
+
+        private string _resourceId;
+        public string ResourceId
+        {
+            get
+            {
+                return _resourceId;
+            }
+
+            set
+            {
+                if (_resourceId != value)
+                {
+                    _resourceId = value;
+                    OnPropertyChanged(() => ResourceId);
+                }
+            }
+        }
+
+
+        private int? _afterShipLv;
+        public int? AfterShipLv
+        {
+            get
+            {
+                return _afterShipLv;
+            }
+
+            set
+            {
+                if (_afterShipLv != value)
+                {
+                    _afterShipLv = value;
+                    OnPropertyChanged(() => AfterShipLv);
+                }
+            }
+        }
+
+
+        private string _afterShipName;
+        public string AfterShipName
+        {
+            get
+            {
+                return _afterShipName;
+            }
+
+            set
+            {
+                if (_afterShipName != value)
+                {
+                    _afterShipName = value;
+                    OnPropertyChanged(() => AfterShipName);
+                }
+            }
+        }
+
+    }
+}
--- Miotsukushi/ViewModel/CheatWindow/ShipMasterListViewModel.cs
+++ Miotsukushi/ViewModel/CheatWindow/ShipMasterListViewModel.cs
@@ -1,197 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
 
 namespace Miotsukushi.ViewModel.CheatWindow
 {
     class ShipMasterListViewModel : ViewModelBase
     {
-        public ObservableCollection<ShipMasterItem> ShipList { get; set; }
+        readonly ObservableCollection<ShipMasterItem> _shipList;
+
+        public ICollectionView ShipListCollection { get; }
+
+        private string _searchText = "";
+        public string SearchText
+        {
+            get
+            {
+                return _searchText;
+            }
+
+            set
+            {
+                if (_searchText != value)
+                {
+                    _searchText = value;
+                    OnPropertyChanged(() => SearchText);
+                    ShipListCollection.Refresh();
+                }
+            }
+        }
 
         public ShipMasterListViewModel()
         {
-            ShipList = new ObservableCollection<ShipMasterItem>();
-            System.Windows.Data.BindingOperations.EnableCollectionSynchronization(ShipList, new object());
+            _shipList = new ObservableCollection<ShipMasterItem>();
+            BindingOperations.EnableCollectionSynchronization(_shipList, new object());
             var kcmodel = Model.MainModel.Current.KancolleModel;
             if (kcmodel.InitializeCompleted)
                 MakeTable();
             else
-                kcmodel.InitializeComplete += (_, __) => MakeTable();   
+                kcmodel.InitializeComplete += (_, __) => MakeTable();
+
+            ShipListCollection = CollectionViewSource.GetDefaultView(_shipList);
+            
+            ShipListCollection.Filter += o =>
+            {
+                var shipMasterItem = o as ShipMasterItem;
+                if (shipMasterItem?.Name != null)
+                {
+                    return shipMasterItem.Name.Contains(SearchText);
+                }
+                return true;
+            };
         }
 
         private void MakeTable()
         {
             var kcmodel = Model.MainModel.Current.KancolleModel;
             foreach (var c in kcmodel.Charamaster)
-                ShipList.Add(new ShipMasterItem()
+                _shipList.Add(new ShipMasterItem
                 {
-                    ID = c.Key,
+                    Id = c.Key,
                     Name = c.Value.Name,
                     ShipType = kcmodel.Shiptypemaster.ContainsKey(c.Value.Shiptype) ? kcmodel.Shiptypemaster[c.Value.Shiptype].Name : "不明",
-                    ShipTypeID = c.Value.Shiptype,
+                    ShipTypeId = c.Value.Shiptype,
                     Yomi = c.Value.NameYomi,
-                    ResourceID = c.Value.ResourceId,
+                    ResourceId = c.Value.ResourceId,
                     AfterShipLv = c.Value.AftershipLv,
                     AfterShipName = c.Value.AftershipId.HasValue ? kcmodel.Charamaster[c.Value.AftershipId.Value].Name : null
                 });
         }
     }
 
-    class ShipMasterItem : ViewModelBase
-    {
-        private int _ID;
-        public int ID
-        {
-            get
-            {
-                return _ID;
-            }
-
-            set
-            {
-                if (_ID != value)
-                {
-                    _ID = value;
-                    OnPropertyChanged(() => ID);
-                }
-            }
-        }
-
-
-        private string _Name;
-        public string Name
-        {
-            get
-            {
-                return _Name;
-            }
-
-            set
-            {
-                if (_Name != value)
-                {
-                    _Name = value;
-                    OnPropertyChanged(() => Name);
-                }
-            }
-        }
-
-
-        private string _ShipType;
-        public string ShipType
-        {
-            get
-            {
-                return _ShipType;
-            }
-
-            set
-            {
-                if (_ShipType != value)
-                {
-                    _ShipType = value;
-                    OnPropertyChanged(() => ShipType);
-                }
-            }
-        }
-
-
-        private int _ShipTypeID;
-        public int ShipTypeID
-        {
-            get
-            {
-                return _ShipTypeID;
-            }
-
-            set
-            {
-                if (_ShipTypeID != value)
-                {
-                    _ShipTypeID = value;
-                    OnPropertyChanged(() => ShipTypeID);
-                }
-            }
-        }
-
-
-        private string _Yomi;
-        public string Yomi
-        {
-            get
-            {
-                return _Yomi;
-            }
-
-            set
-            {
-                if (_Yomi != value)
-                {
-                    _Yomi = value;
-                    OnPropertyChanged(() => Yomi);
-                }
-            }
-        }
-
-
-        private string _ResourceID;
-        public string ResourceID
-        {
-            get
-            {
-                return _ResourceID;
-            }
-
-            set
-            {
-                if (_ResourceID != value)
-                {
-                    _ResourceID = value;
-                    OnPropertyChanged(() => ResourceID);
-                }
-            }
-        }
-
-
-        private int? _AfterShipLv;
-        public int? AfterShipLv
-        {
-            get
-            {
-                return _AfterShipLv;
-            }
-
-            set
-            {
-                if (_AfterShipLv != value)
-                {
-                    _AfterShipLv = value;
-                    OnPropertyChanged(() => AfterShipLv);
-                }
-            }
-        }
-
-
-        private string _AfterShipName;
-        public string AfterShipName
-        {
-            get
-            {
-                return _AfterShipName;
-            }
-
-            set
-            {
-                if (_AfterShipName != value)
-                {
-                    _AfterShipName = value;
-                    OnPropertyChanged(() => AfterShipName);
-                }
-            }
-        }
-
-    }
 }
