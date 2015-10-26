75a0fcc2663ee5d28b9fb7d7c4e4c1482b9a8539
Ishotihadus <hanachan.pao@gmail.com>
2015-10-27 08:58:43 +0900

    ShipListItemViewのクラスを作成

---
+++ Miotsukushi/ViewModel/CheatWindow/ShipListItemViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Miotsukushi.ViewModel.CheatWindow
+{
+    class ShipListItemViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int ShipId
+        {
+            get
+            {
+                return _shipId;
+            }
+
+            set
+            {
+                if (_shipId == value) return;
+                _shipId = value;
+                OnPropertyChanged(() => ShipId);
+            }
+        }
+
+        private int _shipId;
+    }
+}
