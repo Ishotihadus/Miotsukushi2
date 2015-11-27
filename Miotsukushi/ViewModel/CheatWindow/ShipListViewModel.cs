38dad792bb1335655bc1b82bbb8924ca72b137f1
Ishotihadus <hanachan.pao@gmail.com>
2015-11-27 09:36:10 +0900

    ShipListViewModelのクラスを作成

---
+++ Miotsukushi/ViewModel/CheatWindow/ShipListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Miotsukushi.ViewModel.CheatWindow
+{
+    class ShipListViewModel : ViewModelBase
+    {
+        public ObservableCollection<ShipListItemViewModel> List { get; private set; }  = new ObservableCollection<ShipListItemViewModel>();
+    }
+}
