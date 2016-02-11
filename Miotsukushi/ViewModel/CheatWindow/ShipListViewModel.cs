c2a3ab2938c833c710898f25a1b4f35a9650a756
Ishotihadus <hanachan.pao@gmail.com>
2016-02-11 12:54:40 +0900

    ShipListViewModelで2つのスレッドからそれぞれListへの操作が起こるバグを修正

--- Miotsukushi/ViewModel/CheatWindow/ShipListViewModel.cs
+++ Miotsukushi/ViewModel/CheatWindow/ShipListViewModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Windows;
-using System.Windows.Data;
 using Miotsukushi.Model;
 using Miotsukushi.Model.KanColle;
 
@@ -17,8 +16,6 @@ namespace Miotsukushi.ViewModel.CheatWindow
 
         public ShipListViewModel()
         {
-            BindingOperations.EnableCollectionSynchronization(List, new object());
-
             _kcmodel = MainModel.Current.KancolleModel;
 
             _modelCollection = _kcmodel.Shipdata;
@@ -30,48 +27,33 @@ namespace Miotsukushi.ViewModel.CheatWindow
         private void MakeTable()
         {
             if (!Application.Current.Dispatcher.CheckAccess())
+            {
                 Application.Current.Dispatcher.Invoke(MakeTable);
+                return;
+            }
 
             foreach (var shipData in _modelCollection)
                 List.Add(new ShipListItemViewModel(_kcmodel, shipData));
         }
 
-        private void AddToTable(ShipData shipData, int pos)
-        {
-            if (!Application.Current.Dispatcher.CheckAccess())
-                Application.Current.Dispatcher.Invoke(() => AddToTable(shipData, pos));
-
-            List.Insert(pos, new ShipListItemViewModel(_kcmodel, shipData));
-        }
-
-        private void MoveTable(int from, int to)
-        {
-            if (!Application.Current.Dispatcher.CheckAccess())
-                Application.Current.Dispatcher.Invoke(() => MoveTable(from, to));
-
-            List.Move(from, to);
-        }
-
-        private void RemoveTable(int index)
-        {
-            if (!Application.Current.Dispatcher.CheckAccess())
-                Application.Current.Dispatcher.Invoke(() => RemoveTable(index));
-
-            List.RemoveAt(index);
-        }
-
         private void _modelCollection_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
+            if (!Application.Current.Dispatcher.CheckAccess())
+            {
+                Application.Current.Dispatcher.Invoke(() => _modelCollection_CollectionChanged(sender, e));
+                return;
+            }
+
             switch (e.Action)
             {
                 case NotifyCollectionChangedAction.Add:
-                    AddToTable(_modelCollection[e.NewStartingIndex], e.NewStartingIndex);
+                    List.Insert(e.NewStartingIndex, new ShipListItemViewModel(_kcmodel, _modelCollection[e.NewStartingIndex]));
                     break;
                 case NotifyCollectionChangedAction.Move:
-                    MoveTable(e.OldStartingIndex, e.NewStartingIndex);
+                    List.Move(e.OldStartingIndex, e.NewStartingIndex);
                     break;
                 case NotifyCollectionChangedAction.Remove:
-                    RemoveTable(e.OldStartingIndex);
+                    List.RemoveAt(e.OldStartingIndex);
                     break;
                 case NotifyCollectionChangedAction.Replace:
                     break;
